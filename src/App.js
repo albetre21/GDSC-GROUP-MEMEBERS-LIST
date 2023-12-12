ead7f24f601b4251adcf42aa6faa996c7b522fdb
@albetre21 <betrearon125@gmail.com>
2023-12-12 20:58:54 +0300

    GDSC - Week 4 -project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Groupmembers from './GroupMembers';
+
+function App() {
+  return (
+    <div className="App">
+      <GroupList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/GroupMembers.js
@@ -0,0 +1,37 @@
+
+import React from 'react';
+import './GroupMembers.css'; // Import your CSS file
+
+const GroupMembers = () => {
+  // List of group members
+  const members = [
+    'Betrearon Dereje',
+    'Albetre Deinstein',
+    'Fiker Dereje ',
+    'Anchalem yazachew',
+    'Dereje Tiruneh',
+    'betelehem liknaw',
+    'Selam biniyam',
+    'Sumueal Alemu',
+    'fiker yene',
+    'fiker yene sitota'
+  ];
+
+  return (
+    <div className="group-members">
+      <h2>Group Members</h2>
+      <ul>
+        {members.map((member, index) => (
+          <li key={index}>{member}</li>
+        ))}
+      </ul>
+      <img
+        src="space.jpg" 
+        alt="Group members"
+        className="group-image"
+      />
+    </div>
+  );
+};
+
+export default GroupMembers;
